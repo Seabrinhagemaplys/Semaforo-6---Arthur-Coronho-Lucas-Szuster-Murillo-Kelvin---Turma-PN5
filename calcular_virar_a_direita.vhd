fdb0fb4ce90b83ca0874cf553bf84fdad03fddcb
Seabrinhagemaplys <66424663+Seabrinhagemaplys@users.noreply.github.com>
2025-11-28 17:32:55 -0300

    Add VHDL entity for right turn calculation

---
+++ calcular_virar_a_direita.vhd
@@ -0,0 +1,24 @@
+-- Arthur Coronho, Lucas Szuster, Murillo Kelvin - Turma PN5
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity calcular_virar_a_direita is
+  port (
+    -- entradas
+	 clear_assincrono : in std_logic;
+    pedestre         : in std_logic;
+    outro_pedestre   : in std_logic;
+    emergencia       : in std_logic;
+    outra_emergencia : in std_logic;
+    
+    -- saídas
+    virar_a_direita : out std_logic := '0'
+  );
+end entity;
+
+architecture calc of calcular_virar_a_direita is
+begin
+	-- pode virar a direita se nenhum pedestre está atravessando
+	virar_a_direita <= '0' when clear_assincrono = '1' else
+    (not(pedestre) and not(outro_pedestre) and not(emergencia) and not(outra_emergencia));	
+end architecture;
