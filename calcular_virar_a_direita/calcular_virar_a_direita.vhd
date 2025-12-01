2315d38f3803c05d53baaa3666e2f2255c0121d4
Seabrinhagemaplys <66424663+Seabrinhagemaplys@users.noreply.github.com>
2025-12-01 01:38:09 -0300

    Update calcular_virar_a_direita.vhd

--- calcular_virar_a_direita/calcular_virar_a_direita.vhd
+++ calcular_virar_a_direita/calcular_virar_a_direita.vhd
@@ -3,22 +3,22 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity calcular_virar_a_direita is
-  port (
-    -- entradas
-	 clear_assincrono : in std_logic;
-    pedestre         : in std_logic;
-    outro_pedestre   : in std_logic;
-    emergencia       : in std_logic;
-    outra_emergencia : in std_logic;
-    
-    -- saídas
-    virar_a_direita : out std_logic := '0'
-  );
+    port (
+        -- entradas
+        clear_assincrono : in std_logic;
+        pedestre         : in std_logic;
+        outro_pedestre   : in std_logic;
+        emergencia       : in std_logic;
+        outra_emergencia : in std_logic;
+        
+        -- saídas
+        virar_a_direita : out std_logic := '0'
+    );
 end entity;
 
 architecture calc of calcular_virar_a_direita is
 begin
-	-- pode virar a direita se nenhum pedestre está atravessando
-	virar_a_direita <= '0' when clear_assincrono = '1' else
-    (not(pedestre) and not(outro_pedestre) and not(emergencia) and not(outra_emergencia));	
+    -- pode virar a direita se nenhum pedestre está atravessando
+    virar_a_direita <= '0' when clear_assincrono = '1' else
+        (not(pedestre) and not(outro_pedestre) and not(emergencia) and not(outra_emergencia));    
 end architecture;
